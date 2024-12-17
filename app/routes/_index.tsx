859b5e76da7659ede633a13b0b2a88fe38303961
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2024-12-17 23:20:00 +0900

    client側でradio絞り込み有効化

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import {
 import { useLoaderData } from '@remix-run/react';
 import { SearchForm } from '~/components/search-form';
 import { type Product } from '~/data/products';
-import { ProductCard } from '~/components/product-card';
+import { ProductList } from '~/components/product-list';
 
 export const loader: LoaderFunction = async ({
 	context,
@@ -32,11 +32,7 @@ export default function Index() {
 		<div className="container mx-auto max-w-5xl p-6 pt-10">
 			<h1 className="mb-6 text-2xl">今日の割引商品</h1>
 			<SearchForm />
-			<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-				{products.map((product: Product) => (
-					<ProductCard key={product.id} product={product} />
-				))}
-			</div>
+			<ProductList products={products} />
 		</div>
 	);
 }
--- app/routes/products._index.tsx
+++ app/routes/products._index.tsx
@@ -1,52 +1,40 @@
-import { LoaderFunctionArgs, json } from '@remix-run/cloudflare';
-import { Link, useLoaderData } from '@remix-run/react';
+import {
+	LoaderFunction,
+	LoaderFunctionArgs,
+	json,
+} from '@remix-run/cloudflare';
+import { useLoaderData } from '@remix-run/react';
 import { SearchForm } from '~/components/search-form';
-import { type Product, products } from '~/data/products';
+import { type Product } from '~/data/products';
 import { PageBreadcrumb } from '~/components/page-breadcrumb';
+import { ProductList } from '~/components/product-list';
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-	const url = new URL(request.url);
-	const search = url.searchParams.get('search');
-	const category = url.searchParams.get('category');
-
-	console.log({
-		url: request.url,
-		search,
-		category,
-	});
-
-	return json({ products });
+export const loader: LoaderFunction = async ({
+	context,
+}: LoaderFunctionArgs) => {
+	try {
+		// TODO: 別ファイルでDBにアクセスする関数を作成し、それを利用する
+		const resolvedContext = await context; // Promiseを解決
+		const products = await resolvedContext.db.product.findMany();
+		return json({ products });
+	} catch (error) {
+		console.error('Failed to load products:', error);
+		throw new Response('Internal Server Error', { status: 500 });
+	}
 };
 
 // 当日の割引商品を表示する
 export default function Index() {
-	const { products } = useLoaderData<typeof loader>();
+	const { products } = useLoaderData<typeof loader>() as {
+		products: Product[];
+	};
 
 	return (
 		<div className="container mx-auto max-w-5xl p-6">
 			<PageBreadcrumb currentPage="商品検索" items={[]} />
 			<h1 className="mb-6 text-2xl">商品一覧</h1>
 			<SearchForm />
-			<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-				{products.map((product: Product) => (
-					<Link
-						key={product.id}
-						to={`/products/${product.id}`}
-						className="block"
-					>
-						<div className="overflow-hidden rounded-lg border border-gray-200 transition-shadow duration-300 hover:shadow-lg">
-							<img
-								src={product.imageUrl}
-								alt={product.name}
-								className="h-90 w-full object-cover"
-							/>
-							<div className="p-4">
-								<h2 className="text-l mb-2">{product.name}</h2>
-							</div>
-						</div>
-					</Link>
-				))}
-			</div>
+			<ProductList products={products} />
 		</div>
 	);
 }
